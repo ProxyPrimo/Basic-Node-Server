b65410df0205162bb96ca2a74c3660d737e38927
Ivalin Kolev <ivalinkolev0@gmail.com>
2021-07-09 22:28:32 +0300

    Added a post request logic for /message

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
 
     if (url === "/message") {
-    res.write("<html>");
-    res.write("<head><title>My Node.js Server</title></head>");
-    res.write("<body>");
-    res.write('<form action="/message" method="POST">');
-    res.write('<input type="text"><button type="submit">Submit</button></input>')
-    res.write("</form>");
-    res.write("</body>");
-    res.write("</html>");
-    return res.end();
+        if (method === "POST") {
+            fs.writeFileSync("message.txt", "DUMMY");
+            res.writeHead(302, {'Location': '/'});
+            return res.end();
+        }
+        res.write("<html>");
+        res.write("<head><title>My Node.js Server</title></head>");
+        res.write("<body>");
+        res.write('<form action="/message" method="POST">');
+        res.write('<input type="text"><button type="submit">Submit</button></input>')
+        res.write("</form>");
+        res.write("</body>");
+        res.write("</html>");
+        return res.end();
     }
 
     res.write("<html>");

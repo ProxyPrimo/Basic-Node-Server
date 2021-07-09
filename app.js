89bbd9395321538c8291c93e3b87ff07e380c879
Ivalin Kolev <ivalinkolev0@gmail.com>
2021-07-09 22:21:52 +0300

    Added some basic routing

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+    const url = req.url;
 
+    if (url === "/message") {
     res.write("<html>");
     res.write("<head><title>My Node.js Server</title></head>");
     res.write("<body>");
     res.write('<form action="/message" method="POST">');
     res.write('<input type="text"><button type="submit">Submit</button></input>')
     res.write("</form>");
+    res.write("</body>");
+    res.write("</html>");
+    res.end();
+    }
+
+    res.write("<html>");
+    res.write("<head><title>My Node.js Server</title></head>");
+    res.write("<body>");
+    res.write("<h2>Hi! This is my basic Node.js server.</h2>")
+    res.write("<p>Go to /message for my form</p>")
     res.write("</body>");
     res.write("</html>");
     res.end();

f2496c1cf64e0a3984312003d788003292efc2fe
Ivalin Kolev <ivalinkolev0@gmail.com>
2021-07-09 22:17:45 +0300

    Created basic Node.js server

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+
+    res.write("<html>");
+    res.write("<head><title>My Node.js Server</title></head>");
+    res.write("<body>");
+    res.write('<form action="/message" method="POST">');
+    res.write('<input type="text"><button type="submit">Submit</button></input>')
+    res.write("</form>");
+    res.write("</body>");
+    res.write("</html>");
+});
+
+
+server.listen(3000);
